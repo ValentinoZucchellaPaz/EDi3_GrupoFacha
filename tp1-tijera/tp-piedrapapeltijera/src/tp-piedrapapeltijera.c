c1726e1885c6dd40b68e9eb22b98b03ac8ca40f4
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-03-19 13:00:02 -0300

    debounce

--- tp1-tijera/tp-piedrapapeltijera/src/tp-piedrapapeltijera.c
+++ tp1-tijera/tp-piedrapapeltijera/src/tp-piedrapapeltijera.c
@@ -23,31 +23,45 @@ int resultado[3][3] = {
 int mapa[8] = { -1, 0, 1, -1, 2, -1, -1, -1 };
 
 
-void delay(void) {
-    for (volatile uint32_t i = 0; i < 1000000; i++);
+void delay(int n) {
+    for (volatile int i = 0; i < n; i++); // 6000000 recomendado largo
+}
+
+int debounce(){
+    int e1, e2;
+
+    do {
+        e1=LPC_GPIO0->FIOPIN & 0x07;
+        delay(50000);
+        e2=LPC_GPIO0->FIOPIN & 0x07;
+    } while(e1!=e2);
+
+    return e1;
 }
 
 int main(void){
 
-	uint32_t relojCpu = SystemCoreClock;
+	// int relojCpu = SystemCoreClock;
 
     // no pongo como GPIO xq ya vienen asi
 	LPC_GPIO0->FIODIR &= ~(0x07);   // entradas (bits 0-2)
     LPC_GPIO0->FIODIR |=  (0x70);   // salidas (bits 4-6) (EMPATE, GANA JUGADOR, GANA CPU)
 
-    uint32_t entrada;
-    uint32_t jugador;
-    uint32_t cpu;
-    uint32_t res;
+    int entrada;
+    int jugador;
+    int cpu;
+    int res;
 
 	while(1){
 
         // leo entrada
-		entrada = LCP_GPIO0->FIOPIN && 0x07;
+		entrada = LPC_GPIO0->FIOPIN & 0x07;
 
         if (entrada == 0) {
             continue;
         }
+
+        entrada = debounce();
 
         // hubo una entrada, me fijo si es valida
         jugador = mapa[entrada];
@@ -56,13 +70,13 @@ int main(void){
             continue; // entrada invalida
         }
 
-        // jugada valida de jugadory hago jugada de cpu
+        // jugada valida de jugador y hago jugada de cpu
         cpu = rand() % 3;
 
         // me fijo res en matriz, muestro en leds y luego semihost
         res = resultado[jugador][cpu];
 
-        LPC_GPIO0->FIOCLR = (7<<4);
+        LPC_GPIO0->FIOCLR = 0x70;
         if (res == 0) { // empate
             LPC_GPIO0->FIOSET |= (1<<4);
         } else if (res == 1) { // gana jugador
@@ -73,10 +87,12 @@ int main(void){
 
         printf("Jugador: %d | CPU: %d | Resultado: %d\n", jugador, cpu, res);
 
-        delay();
+        delay(200000);
 
         // vuelve a arrancar cuando se suelta boton, sino no
         while((LPC_GPIO0->FIOPIN & 0x07) != 0);
+
+        delay(50000);
 	}
 
 	return 0;
