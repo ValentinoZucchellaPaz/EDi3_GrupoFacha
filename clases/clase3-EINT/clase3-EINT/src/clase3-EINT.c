f7c28334bee6a9bb07de3f7c8b28098a265e4edf
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-03-24 01:47:38 -0300

    fix/EINT0 - Faltaba configurar pin de interrupcion (P2.10) como entrada

--- clases/clase3-EINT/clase3-EINT/src/clase3-EINT.c
+++ clases/clase3-EINT/clase3-EINT/src/clase3-EINT.c
@@ -4,17 +4,21 @@
 
 void EINT0_IRQHandler(void) {
 	LPC_GPIO0->FIOPIN ^= (1<<22); // toggle de led
-	LPC_SC->EXTINT = (1<<0); // bajo bandera
+	LPC_SC->EXTINT |= (1<<0); // bajo bandera
 }
 
 int main(void) {
 	// codigo para poner un boton en P2.10 (4to pin der contando desde abajo) conectado a VCC (primer pin der contando desde arriba)
 	// dispara interrupcion que prende y apaga led
-
-
+	
+	LPC_PINCON->PINSEL1 &= ~(3 << 12); // config P0.22 como GPIO, viene con reset
 	LPC_GPIO0->FIODIR |= (1<<22); // led (P0.22) como salida
 
-	// config pin de entrada (P2.10) como EINT0
+
+	// config P2.10 como entrada
+	LPC_GPIO2->FIODIR &= ~(1<<10);
+
+	// config P2.10 como EINT0
 	LPC_PINCON->PINSEL4 &= ~(3<<20); // limpio bits
 	LPC_PINCON->PINSEL4 |= (1<<20);
 
@@ -27,7 +31,7 @@ int main(void) {
 	LPC_SC->EXTPOLAR |= (1<<0); // asc
 
 	// Limpiar cualquier interrupción pendiente
-    LPC_SC->EXTINT = (1 << 0);
+    LPC_SC->EXTINT |= (1 << 0);
 
 	// habilito int en NVIC - por funcion (no vimos todavia)
 	//NVIC_EnableIRQ(EINT0_IRQn);
