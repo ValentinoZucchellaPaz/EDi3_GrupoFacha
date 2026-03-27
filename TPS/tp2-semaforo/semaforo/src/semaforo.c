82af5c9a846b9f916f7a30569b7f5a43a653404d
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-03-27 16:15:57 -0300

    tp/semaforo - por fin anda para la consigna de Ian, Blasco tiene otra consigna mas compleja

--- TPS/tp2-semaforo/semaforo/src/semaforo.c
+++ TPS/tp2-semaforo/semaforo/src/semaforo.c
@@ -22,44 +22,56 @@ int main(void) {
 	SysTick_Config(SystemCoreClock/1000);//config 1ms
 
 	// config e inicializacion de leds
-	LPC_PINCON->PINSEL1 &= ~((3 << 12) | (3 << 18) | (3 << 20)); // pongo como gpio
-	LPC_GPIO0->FIODIR |= (1<<22) | (1<<25) | (1<<26); // led rojo, verde, azul
-	LPC_GPIO0->FIOSET |= (1<<22) | (1<<25) | (1<<26); // inicializo con leds apagados (logica negativa)
+	// LPC_PINCON->PINSEL1 &= ~(3 << 12); // rojo
+	// LPC_PINCON->PINSEL7 &= ~(3<<18); // verde
+	// LPC_PINCON->PINSEL7 &= ~(3<<20); // azul
+
+	// pongo como salida
+	LPC_GPIO0->FIODIR |= (1<<22); // rojo
+	LPC_GPIO3->FIODIR |= (1<<25); // verde
+	LPC_GPIO3->FIODIR |= (1<<26); // azul
+
+	// leds apagados (logica neg)
+	LPC_GPIO0->FIOSET |= (1<<22); // rojo
+	LPC_GPIO3->FIOSET |= (1<<25); // verde
+	LPC_GPIO3->FIOSET |= (1<<26); // azul
 
 
     while(1) {
-    	estado_actual = (contador_ms < 1000) ? ROJO :
-    	         (contador_ms < 3000) ? VERDE :
-    	         (contador_ms < 6000) ? AZUL : ROJO;
+    	estado_actual = (contador_ms < 4000) ? ROJO :
+    	         (contador_ms < 8000) ? VERDE :
+    	         (contador_ms < 10000) ? AZUL : ROJO;
 
-    	if(contador_ms >= 6000){
+    	if(contador_ms >= 10000){
 			contador_ms=0;
 		}
 
     	if (estado_actual != estado_anterior){
     		estado_anterior = estado_actual;
 
+
     		switch(estado_actual){
     			case ROJO:
-					// pasa a verde
-					LPC_GPIO0->FIOSET |= (1<<25) | (1<<26);
+					// pasa a rojo
+					LPC_GPIO3->FIOSET |= (1<<26) | (1<<25);
 					LPC_GPIO0->FIOCLR |= (1<<22);
 					break;
 
 				case VERDE:
-					// pasa a azul
-					LPC_GPIO0->FIOSET |= (1<<22) | (1<<26);
-					LPC_GPIO0->FIOCLR |= (1<<25);
+					// pasa a verde
+					LPC_GPIO0->FIOSET |= (1<<22);
+					LPC_GPIO3->FIOSET |= (1<<26);
+					LPC_GPIO3->FIOCLR |= (1<<25);
 					break;
 
 				case AZUL:
-					// pasa a rojo
-					LPC_GPIO0->FIOSET |= (1<<22) | (1<<25);
-					LPC_GPIO0->FIOCLR |= (1<<26);
+					// pasa a azul
+					LPC_GPIO0->FIOSET |= (1<<22);
+					LPC_GPIO3->FIOSET |= (1<<25);
+					LPC_GPIO3->FIOCLR |= (1<<26);
 					break;
     		}
     	}
-
     }
     return 0 ;
 }
