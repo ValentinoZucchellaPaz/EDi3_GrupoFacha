c98566ce9c124cb559359e6f8a957931dd501a8c
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-04-20 10:47:28 -0300

    refactor: tp leds fade in/out - pasar logica de ISR a main con banderas

--- TPS/tp3-timer_pwm/led_fade_in_out.c
+++ TPS/tp3-timer_pwm/led_fade_in_out.c
@@ -11,7 +11,7 @@ Esto resulta en fadein completo en 1seg
 #include "LPC17xx.h"
 #endif
 
-#include <cr_section_macros.h>
+// #include <cr_section_macros.h>
 
 #include <stdio.h>
 
@@ -20,9 +20,9 @@ Esto resulta en fadein completo en 1seg
 #define DUTY_STEP     250        // paso de incremento (250 ticks = 10us ~ 1% de 1ms)
 #define PERIODS_10MS  10         // 10 periodos de 1ms = 10ms
 
-// Variable global del duty cycle actual
-volatile uint32_t duty = 0;
-volatile uint8_t fade_in = 1;    // 1 = fade in, 0 = fade out
+// Flags para duty cycle
+volatile uint32_t flag_periodo = 0;
+volatile uint32_t apagar_led = 0;
 
 void led_init(void){
     // LED de P0.22 como salida GPIO, comienza off
@@ -38,7 +38,7 @@ void timer_init(void) {
     // Config: reseteo, sin prescaler, cargo registros de match
     LPC_TIM0->TCR = 0x02;
     LPC_TIM0->PR  = 0;
-    LPC_TIM0->MR0 = duty;
+    LPC_TIM0->MR0 = 0; // duty cycle, varia (comienza en 0)
     LPC_TIM0->MR1 = MR1_PERIOD;
 
     // Match Control Register
@@ -58,9 +58,54 @@ void timer_init(void) {
 int main(void) {
     led_init();
     timer_init();
+    uint8_t contador = 0;
+    uint32_t duty = 0;
+    uint8_t fade_in = 1;    // 1 = fade in, 0 = fade out
 
     while(1) {
-        // idle
+        if (apagar_led==1)
+        {
+            LPC_GPIO0->FIOSET = (1 << 22);
+            apagar_led=0;
+        }
+
+        if(flag_periodo==1)
+        {
+            // cuento 10 MR1 (10ms) entonces:
+            // 1. actualizo duty cycle segun fade in/out
+            // 2. prendo led (inicio de nuevo periodo)
+            // incrementos (fade in) y decrementos (fade out) se hacen con saltos (DUTY_STEP)
+
+            flag_periodo=0;
+            contador++;
+            
+            if (contador >= PERIODS_10MS) {
+                contador = 0;
+
+                if (duty > 0)
+                    LPC_GPIO0->FIOCLR = (1 << 22); // prendo led
+
+                if (fade_in) {
+                    duty += DUTY_STEP;
+                    // si llego a max (MR1) comienzo a hacer fade out
+                    if (duty >= MR1_PERIOD) {
+                        duty    = MR1_PERIOD;
+                        fade_in = 0;
+                    }
+                } else {
+                    // si llego a min (MR0 inicial) comienzo a hacer fade in
+                    if (duty <= DUTY_STEP) {
+                        duty    = 0;
+                        fade_in = 1;
+                    } else {
+                        duty -= DUTY_STEP;
+                    }
+                }
+
+                // cargo cambios a MR0
+                LPC_TIM0->MR0 = duty;
+            }
+        }
     }
 
     return 0;
@@ -72,43 +117,13 @@ void TIMER0_IRQHandler(void) {
     // MR0 match: apagar LED
     if (LPC_TIM0->IR & (1 << 0)) {
         LPC_TIM0->IR = (1 << 0);
-        LPC_GPIO0->FIOSET = (1 << 22);
+        apagar_led=1;
     }
 
     // MR1 match: inicio de nuevo periodo, encender LED, actualizo periodo de MR0
     if (LPC_TIM0->IR & (1 << 1)) {
-        // limpio flag, prendo led
+        // limpio flag
         LPC_TIM0->IR = (1 << 1);
-        if (duty > 0)
-            LPC_GPIO0->FIOCLR = (1 << 22);
-
-        // cuento 10 MR1 (10ms) entonces actualizo duty cycle segun fade in/out
-        // incrementos (fade in) y decrementos (fade out) se hacen con saltos (DUTY_STEP)
-        static uint8_t contador = 0;
-        contador++;
-
-        if (contador >= PERIODS_10MS) {
-            contador = 0;
-
-            if (fade_in) {
-                duty += DUTY_STEP;
-                // si llego a max (MR1) comienzo a hacer fade out
-                if (duty >= MR1_PERIOD) {
-                    duty    = MR1_PERIOD;
-                    fade_in = 0;
-                }
-            } else {
-                // si llego a min (MR0 inicial) comienzo a hacer fade in
-                if (duty <= DUTY_STEP) {
-                    duty    = 0;
-                    fade_in = 1;
-                } else {
-                    duty -= DUTY_STEP;
-                }
-            }
-
-            // cargo cambios a MR0
-            LPC_TIM0->MR0 = duty;
-        }
+        flag_periodo = 1;
     }
 }
