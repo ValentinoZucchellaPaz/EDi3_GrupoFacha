011f7bfe1372375f7ad6795189e5b5917badc4e7
ValentinoZucchellaPaz <vzucchellapaz@gmail.com>
2026-05-07 18:19:19 -0300

    fix: output signal via DAC corrections

--- clases/clase8-DAC/ejercicio_onda_dac.c
+++ clases/clase8-DAC/ejercicio_onda_dac.c
@@ -11,7 +11,6 @@ void configDAC(void);
 void configTimer(void);
 
 int main(void) {
-
     configDAC();
     configTimer();
     while(1){
@@ -23,30 +22,20 @@ void configTimer(void) {
     // config prescaler struct
     TIM_TIMERCFG_T prescalerConfig;
     prescalerConfig.prescaleOpt = TIM_US;
-    prescalerConfig.prescaleValue = 100;
+    prescalerConfig.prescaleValue = 10;
 
     // config match0 struct
     TIM_MATCHCFG_T match0Config;
     match0Config.channel = TIM_MATCH_0;
     match0Config.extOpt = TIM_NOTHING;
     match0Config.intEn = ENABLE;
-    match0Config.resetEn = DISABLE;
+    match0Config.resetEn = ENABLE;
     match0Config.stopEn = DISABLE;
-    match0Config.matchValue = 1;
-
-    // config match1 struct
-    TIM_MATCHCFG_T match1Config;
-    match1Config.channel = TIM_MATCH_1;
-    match1Config.extOpt = TIM_NOTHING;
-    match1Config.intEn = DISABLE;
-    match1Config.resetEn = ENABLE;
-    match1Config.stopEn = DISABLE;
-    match1Config.matchValue = 10;
+    match0Config.matchValue = 9;
 
 
     TIM_InitTimer(LPC_TIM0, &prescalerConfig);
     TIM_ConfigMatch(LPC_TIM0, &match0Config);
-    TIM_ConfigMatch(LPC_TIM0, &match1Config);
     NVIC_EnableIRQ(TIMER0_IRQn);
     TIM_Enable(LPC_TIM0);
 }
